e4e9cd1f0efb7f1b69aa3c9379ac2ed1cf070a91
weilixu <weilix@andrew.cmu.edu>
2015-08-28 08:52:33 -0400

    set up district heat system

--- src/baseline/hvac/system7/DistrictHeatHVACSystem7.java
+++ src/baseline/hvac/system7/DistrictHeatHVACSystem7.java
@@ -4,22 +4,38 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import baseline.generator.EplusObject;
+import baseline.idfdata.EnergyPlusBuilding;
 
 /**
  * decorator class, decorates the system with purchased heat system
+ * G3.1.1.3.1: If the proposed building design uses purchased heat, but does not use purchased chilled water,
+ * then Table 3.1.1A and Table G3.1.1B shall be used to select the baseline HVAC system Type and purchased heat shall be substituted
+ * for the Heating Type in Table G3.1.1B. The same heating source shall be used in the proposed and baseline building design.
+ * 
+ * The pump power shall be 14W/gpm.
  * @author Weili
  *
  */
 public class DistrictHeatHVACSystem7 implements SystemType7{
     private SystemType7 system;
+    // recording all the required data for HVAC system type 7
+    private HashMap<String, ArrayList<EplusObject>> objectLists;
+    private final EnergyPlusBuilding building;
+
     
-    public DistrictHeatHVACSystem7(SystemType7 system){
-	this.system = system;
+    public DistrictHeatHVACSystem7(SystemType7 sys, EnergyPlusBuilding bldg){
+	this.system = sys;
+	objectLists = system.getSystemData();
+	building = bldg;
+	
     }
     
     @Override
     public HashMap<String, ArrayList<EplusObject>> getSystemData() {
+	return objectLists;
 	
-	return null;
     }
+    
+    
+    
 }
