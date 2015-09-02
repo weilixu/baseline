b94246d4a5d6325d1558e1d0ecf77c875ad62097
weilixu <weilix@andrew.cmu.edu>
2015-09-02 12:00:13 -0400

    add test case

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,8 +11,8 @@ public class Main {
 
     
     public static void main(String[] args) throws IOException{
-	File energyplusFile = new File("C:\\Users\\Weili\\Desktop\\AssetScoreTool\\1MPTest\\1MP.idf");
-	File weatherFile = new File("C:\\Users\\Weili\\Desktop\\AssetScoreTool\\1MPTest\\USA_PA_Pittsburgh-Allegheny.County.AP.725205_TMY3");
+	File energyplusFile = new File("testcase/1MP.idf");
+	File weatherFile = new File("testcase/USA_PA_Pittsburgh-Allegheny.County.AP.725205_TMY3");
 	ClimateZone zone = ClimateZone.CLIMATEZONE5A;
 	
 	Generator generator = new Generator(energyplusFile,weatherFile,zone,false);
