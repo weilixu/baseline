b9436fd4790261e61b249c80c1a40cbe984eb3bf
weilixu <weilix@andrew.cmu.edu>
2015-09-04 14:04:09 -0400

    add documentation

--- src/baseline/idfdata/BuildingConstruction.java
+++ src/baseline/idfdata/BuildingConstruction.java
@@ -4,8 +4,19 @@ import baseline.construction.opaque.OpaqueEnvelopeParser;
 import baseline.util.ClimateZone;
 
 public interface BuildingConstruction {
-    
+    /**
+     * Get the climate zone of the study building.
+     * This returns the numerator Climate Zone
+     * @return
+     */
     public ClimateZone getClimateZone();
     
+    /**
+     * Replace the design case construction with the ASHRAE 90.1
+     * qualified performance constructions
+     * This has to be performed before climate zone is assigned to
+     * the building
+     * @param envelope
+     */
     public void replaceConstruction(OpaqueEnvelopeParser envelope);
 }
