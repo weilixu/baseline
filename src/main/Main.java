c09ad15583d71a62d3e3b2e6bb080d12759de484
weilixu <weilix@andrew.cmu.edu>
2015-10-20 08:42:14 -0400

    remove tests

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,8 +13,8 @@ public class Main {
     public static void main(String[] args) throws IOException{
 	//File energyplusFile = new File("C:\\Users\\Weili\\Desktop\\AssetScoreTool\\1MPTest\\1MP.idf");
 	//File weatherFile = new File("C:\\Uers\\Weili\\Desktop\\AssetScoreTool\\1MPTest\\USA_PA_Pittsburgh-Allegheny.County.AP.725205_TMY3");
-	File energyplusFile = new File("E:\\02_Weili\\01_Projects\\12_ILEED\\Standard_Model\\Automate\\SH_VRF\\Scaife_Hall_Simple_VRF.idf");
-	File weatherFile = new File("E:\\02_Weili\\01_Projects\\12_ILEED\\Standard_Model\\Automate\\SH_VRF\\USA_MD_Baltimore-Washington.Intl.AP.724060_TMY3");
+	File energyplusFile = new File("E:\\02_Weili\\01_Projects\\12_ILEED\\Standard_Model\\Automate\\SH_FCU\\Scaife_Hall_Base.idf");
+	File weatherFile = new File("E:\\02_Weili\\01_Projects\\12_ILEED\\Standard_Model\\Automate\\SH_FCU\\USA_MD_Baltimore-Washington.Intl.AP.724060_TMY3");
 	
 	ClimateZone zone = ClimateZone.CLIMATEZONE4A;
 	String tool = "DesignBuilder";
